4694f881be0b1abba68451f30713fade0018dd3b
JakubCzarlinski <jakubczarlinski@gmail.com>
2023-02-26 17:53:40 +0000

    Complete the naive model.
    This model predicts the mean of all success metrics. It uses the
    correlations from the paper as weights for CSFs and then normalises to
    output to a value between 1.0 and 5.0.
    
    I'll work on a more sophisticated model next.

--- predictions/model.py
+++ predictions/model.py
@@ -6,15 +6,15 @@ project success.
 
 from typing import Dict, List
 
-from critical_success_factors import CSF
+from predictions.critical_success_factors import CSF
+from predictions.success_metrics import SuccessMetric
 
 
 class Model:
 
-    def __init__(self, critical_success_factors: List[CSF]) -> None:
-        """Initialize a model given a list of valid critical success factors."""
-        self.critical_success_factors = critical_success_factors
-
-    def predict(self, critical_success_factors_values: Dict[CSF, int]):
+    def predict(
+        self,
+        critical_success_factors_values: Dict[CSF, int],
+    ) -> Dict[SuccessMetric, float]:
         """Predict success of a project given a mapping of CSFs to values."""
         raise NotImplementedError("Model.predict() not implemented.")
--- predictions/model_naive.py
+++ predictions/model_naive.py
@@ -5,16 +5,59 @@ correlations with the mean of the project's success metrics.
 
 import typing
 
-from predictions.critical_success_factors import CSF
+from predictions.critical_success_factors import CSF, OF4, OF5, OF6, TF1, TF2, PF5
 from predictions.model import Model
+from predictions.success_metrics import SuccessMetric, OVERALL
+from predictions.utils import create_prediction_map
 
 
 class ModelNaive(Model):
+    """
+    A naive model that uses a weighted sum of the Critical Success Factors and
+    their correlations with the mean of the project's success metrics.
+    """
 
-    def __init__(self, correlations: typing.Dict[CSF, float]):
-        # TODO(czarlinski): Implement this function.
-        self.correlations = correlations
+    def __init__(self):
+        self.correlations = {
+            OF4: 0.29,
+            OF5: 0.32,
+            OF6: 0.29,
+            TF1: 0.22,
+            TF2: 0.27,
+            PF5: -0.10,
+        }
 
-    def predict(self, critical_success_factors_values: typing.Dict[CSF, int]):
-        # TODO(czarlinski): Implement this function.
-        pass
+    def predict(
+        self,
+        critical_success_factors_values: typing.Dict[CSF, int],
+    ) -> typing.Dict[SuccessMetric, float]:
+        # We use OF.04, OF.05, OF.06, TF.01, TF.02 to positively correlate with
+        # the mean of the success metrics. We then use and PF.05 to negatively
+        # correlate with the mean of the success metrics.
+        summed = critical_success_factors_values[OF4] * 0.29
+        summed += critical_success_factors_values[OF5] * 0.32
+        summed += critical_success_factors_values[OF6] * 0.29
+        summed += critical_success_factors_values[TF1] * 0.22
+        summed += critical_success_factors_values[TF2] * 0.27
+
+        summed -= critical_success_factors_values[PF5] * 0.10
+
+        success_metrics = create_prediction_map()
+        success_metrics[OVERALL] = self.normalise(summed)
+
+        return success_metrics
+
+    def normalise(self, value):
+        """Normalise outputs to be between 1 and 5."""
+        self.max_value = 0
+        self.min_value = 0
+        for correlation in self.correlations.values():
+            if correlation > 0:
+                self.max_value += correlation * 5
+                self.min_value += correlation * 1
+            else:
+                self.min_value += correlation * 5
+                self.max_value += correlation * 1
+
+        norm = (value - self.min_value) / (self.max_value - self.min_value)
+        return norm * 4 + 1
