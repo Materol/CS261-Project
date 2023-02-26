f86699a02bedcb50bcc86eea204f0ec19610be4f
Materol <gaarapavel@gmail.com>
2023-02-26 01:39:57 +0000

    Added basic navbar. Will add conditional nav items depending on log in status.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <div className='App'>
       <Router>
+        <NavBar />
         <Routes>
           <Route path='/' element={<h1>Home</h1>} />
           <Route path='/login' element={<Login />} exact />
