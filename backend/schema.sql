7c111e7b0f21ac075bf994f2715cc3a97e2f9f68
Pav-Patra <pavpatra@btinternet.com>
2023-02-27 22:03:40 +0000

    adjusted csf data type to integer and fixed the input test data for this change

--- backend/schema.sql
+++ backend/schema.sql
@@ -16,6 +16,7 @@ DROP TABLE IF EXISTS project_history CASCADE;
     - completed boolean cariable NOT NULL???
     - all project history must be after the start date
     - don't have a reportee reporting to manager while the manager reports to the reportee
+    -- a reportee can also not manage themselves???
 */
 
 /*
@@ -23,10 +24,41 @@ DROP TABLE IF EXISTS project_history CASCADE;
     Look at the UI diagram to get any ideas of this and maybe any of the other diagrams
 */
 
+
+-- edited_on time cannot be earlier than created_on time
+CREATE OR REPLACE FUNCTION check_edit_on() RETURNS trigger AS $$
+DECLARE 
+    projectid INTEGER;
+BEGIN
+    SELECT PH.id FROM project P INNER JOIN project_history PH ON P.id = PH.project_id INTO projectid WHERE NEW.edited_on < P.created_on;
+    IF found THEN
+        RETURN NULL;
+    END IF;
+    RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+
+-- a reportee cannot report to a manager of the manager reports to the reportee
+-- a reportee can also no tmanage themselves
+CREATE OR REPLACE FUNCTION valid_report_manage() RETURNS trigger AS $$
+DECLARE
+    reportee INTEGER;
+    manager INTEGER;
+BEGIN
+    SELECT RT.reportee_id, RT.manager_id FROM reports_to RT INTO reportee, manager WHERE NEW.reportee_id = RT.manager_id AND NEW.manager_id = RT.reportee_id OR NEW.manager_id = NEW.reportee_id;
+    IF found THEN
+        RETURN NULL;
+    END IF;
+    RETURN NEW;
+END 
+$$ LANGUAGE plpgsql;
+
+
+ 
 -- Every project is unique 
 CREATE TABLE project (
     id SERIAL PRIMARY KEY,
-    createdOn TIMESTAMP NOT NULL,
+    created_on TIMESTAMP NOT NULL,
     completed BOOLEAN NOT NULL
 );
 
@@ -36,7 +68,7 @@ CREATE TABLE project_history (
     project_id INTEGER REFERENCES project(id),
     title VARCHAR(100) NOT NULL,
     project_description VARCHAR(800) NOT NULL,
-    csf VARCHAR(100) NOT NULL,
+    csf INTEGER NOT NULL,
     feedback VARCHAR(800) NOT NULL,
     edited_on TIMESTAMP NOT NULL
 );
@@ -75,3 +107,43 @@ CREATE TABLE reports_to (
     reportee_id INTEGER REFERENCES people(id),
     manager_id INTEGER REFERENCES people(id)
 );
+
+------------------------------------------------------------------------------------------------
+-- VIEWS
+------------------------------------------------------------------------------------------------
+-- get person passwords
+CREATE OR REPLACE VIEW get_passwords AS
+SELECT P.id, P.password_hash FROM people P; 
+
+-- get icompleted projects
+CREATE OR REPLACE VIEW uncomplete_project AS
+SELECT P.id, P.created_on FROM project P WHERE completed = 'false';
+
+-- get completed projects
+CREATE OR REPLACE VIEW complete_project AS
+SELECT P.id, P.created_on FROM project P WHERE completed = 'true';
+
+-- get emails of people on a project name with the role of each person
+CREATE OR REPLACE VIEW project_people AS
+SELECT project_id, title, people_id, email, role_title FROM 
+(SELECT project_id, title, members.people_id, role_title FROM
+(SELECT PH.project_id, PH.title, MH.people_id, role_id FROM project_history PH INNER JOIN member_role_history MH ON PH.id = MH.history_id) as members
+INNER JOIN 
+(SELECT id, role_title FROM project_role) as PR
+ON members.role_id = PR.id) AS table1
+INNER JOIN
+(SELECT id, email FROM people) AS table2
+ON table1.people_id = table2.id; 
+
+-----------------------------------------------------------------------------------------------------
+
+-- trigger used to check that the edit_on date for a new project_history field is not before the project's created date
+CREATE TRIGGER check_edit_date BEFORE INSERT OR UPDATE ON project_history
+FOR EACH ROW
+EXECUTE PROCEDURE check_edit_on();
+
+-- trigger used to check that a manager doesn't manage a reportee while the reportee manages the manager at the same time
+-- bug with first data entry failing to check (reportee_id == manager_id)???
+CREATE TRIGGER check_manage_people BEFORE INSERT OR UPDATE ON reports_to
+FOR EACH ROW
+EXECUTE PROCEDURE valid_report_manage();
