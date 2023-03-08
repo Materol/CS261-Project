78c64126a552feaddb3ec7262785971ceb8b2b72
NGWick <123475454+NGWick@users.noreply.github.com>
2023-03-08 15:51:44 +0000

    fixed indentation axios

--- frontend/src/axiosApi.js
+++ frontend/src/axiosApi.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 const axiosInstance = axios.create({
 	baseURL: 'http://127.0.0.1:8000/api/',
 	timeout: 5000,
-    //The header must contain JWT(a space)+access_token for authorization, this is retrieved from localstorage
+    	//The header must contain JWT(a space)+access_token for authorization, this is retrieved from localstorage
 	headers: {
 		Authorization: localStorage.getItem('access_token')
             ? 'JWT ' + localStorage.getItem('access_token')
