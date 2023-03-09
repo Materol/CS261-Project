3ba135fdf9605f63de29f7dec5d973d60be49bb6
Materol <gaarapavel@gmail.com>
2023-03-09 01:38:20 +0000

    Finished Project View Page Made a utils file for seperating functions. Project view page contains project details, overall success metric, the three categories of metrics split up and visualised by proportional bars and then finally, tables of all the metrics and their feedback.

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -3,13 +3,20 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Card, Row, Col, Carousel, Button, Stack } from 'react-bootstrap';
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import { percentToColor } from './Utils';
 import './style/Dashboard.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-circular-progressbar/dist/styles.css';
 // dashboard component
 export default function Dashboard(props) {
     // state variables
-    const [projects, setProjects] = useState([]);
+    const [projects, setProjects] = useState([
+        {id: 1, name: 'SofTrack', description: 'A software for managers to track the development of their software and its success chance.', successChance: 36}, 
+        {id: 2, name: 'WAFFLES', description: 'A restaurant management system for finding, rating, reviewing and filtering.', successChance: 9}, 
+        {id: 3, name: 'Split The House', description: 'A website to manage the bills between students in a household.', successChance: 54}, 
+        {id: 4, name: 'Hurdle', description: 'A wordle solving Haskell program.', successChance: 69},
+        {id: 5, name: 'Packet Sniffer', description: 'A packet intercepter in C to detect and alert of malware passing through a network.', successChance: 87}
+    ]);
 
     // nav hook
     const navigate = useNavigate();
@@ -19,23 +26,16 @@ export default function Dashboard(props) {
         if (props.isLoggedIn == false) {
             navigate('/login');
         }
-        // fetch user's projects
+        // fetch user's projects using their token (localStorage.getItem('access_token'))
         // insert axios call to get user's projects from django backend
         // store in following format into projects via 'setProjects':
         // {id (int), name (string), description (string), successChance (int/100)}
 
 
     }, []);
-    // function to calculate ring colour based on percentage
-    const percentToColor = (percent) => {
-        const x = percent / 100;
-        const r = Math.min(2 - (2 * x), 1) * 255;
-        const g = Math.min(2 * x, 1) * 255;
-        return `rgba(${r}, ${g}, 100, 0.6)`;
-    }
 
     return (
-        <>  
+        <>
             <div className='projectsContainer'>
                 <div className='dashboardHeader'>
                     <span className='projectsTitle'>
@@ -57,24 +57,28 @@ export default function Dashboard(props) {
                                                     <Card className='project'>
                                                         <Card.Body>
                                                             <Card.Title className='projectName'>{project.name}</Card.Title>
-                                                            <hr className='bt-3'/>
+                                                            <hr className='bt-3' />
                                                             <Row md={2}>
                                                                 <Col>
-                                                                    {console.log(percentToColor(project.successChance))}
-                                                                    <CircularProgressbar 
-                                                                        styles={ buildStyles({
+                                                                    <CircularProgressbar
+                                                                        styles={buildStyles({
                                                                             pathColor: percentToColor(project.successChance),
                                                                             textColor: 'black'
                                                                         })
-                                                                        } 
-                                                                        value={project.successChance} text={`${project.successChance}%`} 
+                                                                        }
+                                                                        value={project.successChance} text={`${project.successChance}%`}
                                                                     />
                                                                     <p className='text-center m-0 fs-10'><i>Success Chance</i></p>
                                                                 </Col>
                                                                 <Col>
                                                                     <p>{project.description}</p>
-                                                                    <Button variant='info' onClick={() => 
-                                                                        navigate('/dashboard/projectview', {state: {projectId: project.id}})
+                                                                    <Button variant='info' onClick={() =>
+                                                                        navigate('/dashboard/project', 
+                                                                        { state: { 
+                                                                            projectId: project.id,
+                                                                            projectName: project.name,
+                                                                            projectDescription: project.description,
+                                                                        }})
                                                                     }>View Project</Button>
                                                                 </Col>
                                                             </Row>
@@ -86,24 +90,30 @@ export default function Dashboard(props) {
                                                     <Col md='auto'>
                                                         <Card className='project'>
                                                             <Card.Body>
-                                                                <Card.Title className='projectName'>{projects[index+1].name}</Card.Title>
+                                                                <Card.Title className='projectName'>{projects[index + 1].name}</Card.Title>
                                                                 <hr className='bt-3'></hr>
                                                                 <Row md={2}>
                                                                     <Col>
-                                                                    <CircularProgressbar 
-                                                                        styles={ buildStyles({
-                                                                            pathColor: percentToColor(projects[index+1].successChance),
-                                                                            textColor: 'black'
-                                                                        })
-                                                                        } 
-                                                                        value={projects[index+1].successChance} text={`${projects[index+1].successChance}%`} 
-                                                                    />
+                                                                        <CircularProgressbar
+                                                                            styles={buildStyles({
+                                                                                pathColor: percentToColor(projects[index + 1].successChance),
+                                                                                textColor: 'black'
+                                                                            })
+                                                                            }
+                                                                            value={projects[index + 1].successChance} 
+                                                                            text={`${projects[index + 1].successChance}%`}
+                                                                        />
                                                                         <p className='text-center m-0 fs-10'><i>Success Chance</i></p>
                                                                     </Col>
                                                                     <Col>
-                                                                        <p>{projects[index+1].description}</p>
-                                                                        <Button variant='info' onClick={() => 
-                                                                            navigate('/dashboard/projectview', {state: {projectId: projects[index+1].id}})
+                                                                        <p>{projects[index + 1].description}</p>
+                                                                        <Button variant='info' onClick={() =>
+                                                                            navigate('/dashboard/project', 
+                                                                            { state: { 
+                                                                                projectId: projects[index + 1].id,
+                                                                                projectName: projects[index + 1].name,
+                                                                                projectDescription: projects[index + 1].description,
+                                                                            }})
                                                                         }>View Project</Button>
                                                                     </Col>
                                                                 </Row>
@@ -111,14 +121,14 @@ export default function Dashboard(props) {
                                                         </Card>
                                                     </Col>
                                                 }
-                                            </Row>  
+                                            </Row>
                                         </Carousel.Item>
                                     );
                                 }
-                            })           
+                            })
                         }
                     </Carousel>
-                :
+                    :
                     <div className='noProjects'>
                         <h1>You have no projects yet!</h1>
                         <Button variant='success' onClick={() => navigate('/dashboard/newproject')}>Make one now! 🖥️</Button>
